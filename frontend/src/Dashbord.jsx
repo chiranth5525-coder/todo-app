102ba8f5a249207ae47a3f51c3b18d4b454e7d00
chiranth <chiranth5525@gmail.com>
2026-06-27 11:20:07 +0530

    Fix project issues

--- frontend/src/Dashbord.jsx
+++ frontend/src/Dashbord.jsx
@@ -1,57 +1,69 @@
-import { useEffect ,useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
-function Dashbord() {
+function Dashboard() {
     const [lists, setLists] = useState([]);
     const [title, setTitle] = useState("");
 
-    useEffect(() =>{
+    useEffect(() => {
         setLists([
-            { id: 1,name: "Personal Task"},
-            { id: 2,name: "college name"},
+            { id: 1, name: "Personal Task" },
+            { id: 2, name: "College Task" },
         ]);
-    },[]);
+    }, []);
 
-    const addList = () =>{
-        if(title) return;
+    const addList = () => {
+        if (!title) return;
 
-        const newList ={
+        const newList = {
             id: Date.now(),
-            name; title,
+            name: title,
         };
 
-        setLists([... lists, newList]);
+        setLists([...lists, newList]);
         setTitle("");
-    );
+    };
 
     const deleteList = (id) => {
-        setLists(lists.filter((1)v=> lists.id !== id))
+        setLists(lists.filter((list) => list.id !== id));
     };
 
     return (
-        <div style={{ display: "flex", height: "100vh"}}>
-            {.* SIDEBAR */}
-            <div style {{width: "200px",background: "#111",colour: "#fff", padding: "10px"}}></div>
-            <h3>Todo Lists</h3>
+        <div style={{ display: "flex", height: "100vh" }}>
+            {/* SIDEBAR */}
+            <div
+                style={{
+                    width: "200px",
+                    background: "#111",
+                    color: "#fff",
+                    padding: "10px",
+                }}
+            >
+                <h3>Todo Lists</h3>
 
-            {lists.map(list) => {
-                <div key={(list.id)} style={{ display: "flex", justifycontent: "space-between",margin: "10px 0"}}>
-                    <span>{list.name}</span>
-                    <button onClick-{()=>deleteList.id}}>x</button>
-                </div>
-            ))}
+                {lists.map((list) => (
+                    <div
+                        key={list.id}
+                        style={{
+                            display: "flex",
+                            justifyContent: "space-between",
+                            margin: "10px 0",
+                        }}
+                    >
+                        <span>{list.name}</span>
+                        <button onClick={() => deleteList(list.id)}>X</button>
+                    </div>
+                ))}
 
-            <input
-              placeholder="new list"
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-            />
-        <button onClick={addList}>Add</button>
+                <input
+                    placeholder="New List"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+                <button onClick={addList}>Add</button>
+            </div>
         </div>
+    );
+}
 
-        </div>
-        };
-    }
-
-    export default Dashbord;
-    
+export default Dashboard;
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,73 +1,68 @@
 import { useState } from "react";
-import axis from "axis";
-import {useNavigate, Link } from "react-router-dom";
+import axios from "axios";
+import { useNavigate, Link } from "react-router-dom";
 
-function signup() {
+function Signup() {
     const navigate = useNavigate();
 
-     const [name, setName]=useState("");
-     const [email, setEmail]=useState("");
-     const [password, setpassword]=useState("");
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
-     const handelSignup =async (e) =>{
+    const handelSignup = async (e) => {
         e.preventDefault();
 
-        try{
-            await axios.post("http://localhost:5000/api.signup",{
+        try {
+            await axios.post("http://localhost:5000/api/signup", {
                 name,
                 email,
                 password,
             });
 
-            alert(signup successfull");
+            alert("Signup successful");
             navigate("/");
         } catch (err) {
-            alert(err.respond?.data?.message || "signup failed");
+            alert(err.response?.data?.message || "Signup failed");
         }
     };
-     return {
-        <div style={{ width: "300px", margine: "100px auto"}}>
-        <h2>Signup</h2>
 
-        <form onSubmit={handelSignup}>
-        <input
-        type="text"
-        placeholder="name"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-        style={{ display: "block", margine:"10px 0",width: "100%"}}
-        /> 
+    return (
+        <div style={{ width: "300px", margin: "100px auto" }}>
+            <h2>Signup</h2>
 
-        <input
-        type="email"
-        placeholder="email"
-        value={email}
-        onChange={(e) => setName(e.target.value)}
-        style={{ display: "block", margine:"10px 0",width: "100%"}}
-        />
+            <form onSubmit={handelSignup}>
+                <input
+                    type="text"
+                    placeholder="name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    style={{ display: "block", margin: "10px 0", width: "100%" }}
+                />
 
-        <input
-        type="password"
-        placeholder="password"
-        value={password}
-        onChange={(e) => setName(e.target.value)}
-        style={{ display: "block", margine:"10px 0",width: "100%"}}
-        />
+                <input
+                    type="email"
+                    placeholder="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    style={{ display: "block", margin: "10px 0", width: "100%" }}
+                />
 
-        <button type="submit">signup</button>
-        </form>
+                <input
+                    type="password"
+                    placeholder="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    style={{ display: "block", margin: "10px 0", width: "100%" }}
+                />
 
-        <p>
-        already have account <Link to="/signup">signup</Link>
-        </p>
+                <button type="submit">Signup</button>
+            </form>
+
+            <p>
+                Already have an account? <Link to="/">Login</Link>
+            </p>
         </div>
-     );
-    }
+    );
+}
 
-    export deafult Signup;
-    
-
-
-         
-     }
-
+export default Signup;
